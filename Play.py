5075ebb71a172f8c5bfce6699d0452b6a67a668f
Lnadlav <sonny.vladan@gmail.com>
2023-12-20 19:27:42 +0100

    Optimisation

--- Play.py
+++ Play.py
@@ -1,35 +1,16 @@
-import random
+from random import choice
 
 def shifumi():
-    options = {1: "Pierre", 2: "Feuille", 3: "Ciseaux"}
-    
     while True:
-        print("Choisissez :")
-        print("1. Pierre")
-        print("2. Feuille")
-        print("3. Ciseaux")
+        choix = input("1. Pierre\n2. Feuille\n3. Ciseaux\nVotre choix (1/2/3) : ")
+        if choix not in ["1", "2", "3"]:
+            break
+        
+        joueur, ordi = ("Pierre", "Feuille", "Ciseaux")[int(choix) - 1], choice(("Pierre", "Feuille", "Ciseaux"))
+        resultat = "Égalité !" if joueur == ordi else "Vous avez gagné !" if ((joueur, ordi) in [('Pierre', 'Ciseaux'), ('Feuille', 'Pierre'), ('Ciseaux', 'Feuille')]) else "L'ordinateur a gagné !"
+        print(f"Vous avez choisi : {joueur}\nL'ordinateur a choisi : {ordi}\n{resultat}")
 
-        choix = input("Votre choix (1/2/3) : ")
-
-        if choix not in options:
-            print("Choix invalide. Veuillez choisir 1, 2 ou 3.")
-            continue
-
-        choix_joueur = options[int(choix)]
-        choix_ordi = options[random.randint(1, 3)]
-
-        print(f"Vous avez choisi : {choix_joueur}")
-        print(f"L'ordinateur a choisi : {choix_ordi}")
-
-        if choix_joueur == choix_ordi:
-            print("Égalité !")
-        elif (choix_joueur, choix_ordi) in [("Pierre", "Ciseaux"), ("Feuille", "Pierre"), ("Ciseaux", "Feuille")]:
-            print("Vous avez gagné !")
-        else:
-            print("L'ordinateur a gagné !")
-
-        reponse = input("Voulez-vous rejouer ? (1 pour oui / 2 pour non) : ")
-        if reponse != '1':
+        if input("Rejouer ? (1 pour oui / autre pour non) : ") != '1':
             break
 
 # Appel de la fonction pour jouer
